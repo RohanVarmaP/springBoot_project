f2d5f355a1a6ae811d823a64930cdf54219209fe
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-12-30 12:34:15 +0530

    created a post request

--- src/main/java/com/rohan/springBoot_project/controller/ProductController.java
+++ src/main/java/com/rohan/springBoot_project/controller/ProductController.java
@@ -3,9 +3,7 @@ package com.rohan.springBoot_project.controller;
 import com.rohan.springBoot_project.model.Product;
 import com.rohan.springBoot_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +18,14 @@ public class ProductController {
         return service.getProducts();
     }
 
-    @RequestMapping("/products/{productId}")
+    @GetMapping("/products/{productId}")
     public Product getProductById(@PathVariable int productId){
         return service.getProductById(productId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod){
+//        System.out.println(prod);
+        service.addProduct(prod);
     }
 }
--- src/main/java/com/rohan/springBoot_project/service/ProductService.java
+++ src/main/java/com/rohan/springBoot_project/service/ProductService.java
@@ -3,15 +3,16 @@ package com.rohan.springBoot_project.service;
 import com.rohan.springBoot_project.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 @Service
 public class ProductService {
 
-    List<Product> products= Arrays.asList(
+    List<Product> products= new ArrayList<>(Arrays.asList(
             new Product(101,"Iphone",50000),
             new Product(102,"Samsung",30000),
-            new Product(103,"IQ00",45000));
+            new Product(103,"IQ00",45000)));
 
     public List<Product> getProducts() {
         return products;
@@ -23,5 +24,9 @@ public class ProductService {
                 .filter(prod->prod.getProductId()==productId)
                 .findFirst()
                 .orElse(new Product(100,"no Item",0));
+    }
+
+    public void addProduct(Product prod){
+        products.add(prod);
     }
 }
